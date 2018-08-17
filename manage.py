ba81dd8c24e62111b3947e76c439006c46f4e1d2
zws910 <vincentzheng0910@gmail.com>
2018-08-17 19:36:23 +0800

    add SQLALCHEMY config: mysql/eco_news

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
@@ -7,6 +8,10 @@ class Config():
     """ 项目配置信息 """
     DEBUG = True
 
+    # 数据库MySQL相关设置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/eco_news"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 @app.route('/')
 def hello_world():
@@ -14,6 +19,8 @@ def hello_world():
 
 
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+
 
 if __name__ == '__main__':
     app.run()
