dcfbe15e984cd1f3858768ef95ff04a80e074bcd
zws910 <vincentzheng0910@gmail.com>
2018-08-17 21:19:24 +0800

    move Config to config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+import redis
+
+
+class Config():
+    """ 项目配置信息 """
+    DEBUG = True
+
+    SECRET_KEY = "\xdf\xf4\xaa\x82&QeX\x0f'jQ\x89b\xff\x11MAC\x8bI\x86d\xc1\xd4X\xe3|\xa7\xbfo\xaa7\xb4A\xda \x11\x7f\xba\x1253\x9e\x91%\xb4\x16"
+
+    # 数据库MySQL相关设置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/eco_news"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Redis相关设置
+    REDIS_HOST = '127.0.0.1'
+    REDES_PORT = '6379'
+    # flask_session的配置信息
+    SESSION_TYPE = "redis"
+    SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDES_PORT)
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
--- manage.py
+++ manage.py
@@ -6,26 +6,9 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
 
+from config import Config
+
 app = Flask(__name__)
-
-
-class Config():
-    """ 项目配置信息 """
-    DEBUG = True
-
-    SECRET_KEY = ""
-
-    # 数据库MySQL相关设置
-    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/eco_news"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # Redis相关设置
-    REDIS_HOST = '127.0.0.1'
-    REDES_PORT = '6379'
-    # flask_session的配置信息
-    SESSION_TYPE = "redis"
-    SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
-    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDES_PORT)
-    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 
 @app.route('/')
@@ -45,7 +28,6 @@ Session(app)
 manager = Manager(app)
 Migrate(app, db)
 manager.add_command('db', MigrateCommand)
-
 
 if __name__ == '__main__':
     manager.run()
